4c53e78de45b7a19f54802d5d973c6dac7c62297
Drlynrtsl <126033556+Drlynrtsl@users.noreply.github.com>
2023-02-22 11:22:30 +0800

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.io.*;
 
 public class Employee{
     public static void main(String [] args){
@@ -16,27 +17,27 @@ public class Employee{
         pp_female = in.nextInt();
         System.out.println("Enter the number of resigned males: ");
         r_male = in.nextInt();
-        System.out.println("Enter the number of resigned males: ");
+        System.out.println("Enter the number of resigned females: ");
         r_female = in.nextInt();
         
         int total_new_hire = nh_female + nh_male;
         int total_perma_pos = pp_female + pp_male;
         int total_resigned = r_female + r_male;
-        double nh_percent_male = nh_male/total_new_hire * 100;
-        double nh_percent_female = nh_female/total_new_hire * 100;
-        double pp_percent_male = pp_male/total_perma_pos * 100;
-        double pp_percent_female = pp_female/total_perma_pos * 100;
-        double r_percent_male = r_male/total_resigned * 100;
-        double r_percent_female = r_female/total_resigned * 100;
+        int nh_percent_male = (nh_male * 100) / total_new_hire;
+        int nh_percent_female = (nh_female * 100) / total_new_hire;
+        double pp_percent_male = (pp_male * 100) / total_perma_pos ;
+        double pp_percent_female = (pp_female * 100) / total_perma_pos;
+        double r_percent_male = (r_male * 100) / total_resigned ;
+        double r_percent_female = (r_female * 100 )/ total_resigned;
         
         System.out.println("Number of hired employees " + total_new_hire);
-        System.out.println("Male:" +  nh_percent_male);
-        System.out.println("Female:" +  nh_percent_female);
+        System.out.println("Male:" +  nh_percent_male + " %");
+        System.out.println("Female:" +  nh_percent_female + " %");
         System.out.println("Number of Permanent employees " + total_perma_pos);
-        System.out.println("Male:" +  pp_percent_male);
-        System.out.println("Female:" +  pp_percent_female);
+        System.out.println("Male:" +  pp_percent_male + " %");
+        System.out.println("Female:" +  pp_percent_female + " %");
         System.out.println("Number of Permanent employees " + total_resigned);
-        System.out.println("Male:" +  r_percent_male);
-        System.out.println("Female:" +  r_percent_female);
+        System.out.println("Male:" +  r_percent_male + " %");
+        System.out.println("Female:" +  r_percent_female + " %");
     }
 }
